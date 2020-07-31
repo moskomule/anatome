d8dc1da7184076e2136fac12de4590f0d4a7dfe7
moskomule <hataya@keio.jp>
2020-07-31 23:42:24 +0900

    add autocast

--- anatome/utils.py
+++ anatome/utils.py
@@ -1,12 +1,19 @@
 from typing import Tuple, Callable
 
+import torch
 from torch import nn, Tensor
 
+AUTO_CAST = False
+
+def use_auto_cast():
+    global AUTO_CAST
+    AUTO_CAST = True
 
 def _evaluate(model: nn.Module,
               data: Tuple[Tensor, Tensor],
               criterion: Callable[[Tensor, Tensor], Tensor]
               ) -> float:
     # evaluate model with given data points using the criterion
-    input, target = data
-    return criterion(model(input), target).item()
+    with torch.cuda.amp.autocast(AUTO_CAST):
+        input, target = data
+        return criterion(model(input), target).item()
