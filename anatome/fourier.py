885f24ff68b367a6aefa7a3230302822e47dae36
moskomule <hataya@keio.jp>
2020-08-01 14:12:43 +0900

    fix misbehavior of fourier.py

--- anatome/fourier.py
+++ anatome/fourier.py
@@ -1,11 +1,11 @@
-from typing import Tuple, Callable, Optional
+from typing import Tuple, Callable, Optional, List
 
 import torch
 from torch import nn, Tensor
 from torch.nn import functional as F
 from tqdm import tqdm
 
-from .utils import _evaluate
+from .utils import _evaluate, _normalize, _denormalize
 
 
 def fft_shift(input: torch.Tensor
@@ -43,7 +43,7 @@ def ifft_shift(input: torch.Tensor
 def add_fourier_noise(idx: Tuple[int, int],
                       images: Tensor,
                       norm: float,
-                      size: Optional[Tuple[int, int]] = None
+                      size: Optional[Tuple[int, int]] = None,
                       ) -> Tensor:
     """ Add Fourier noise
 
@@ -63,13 +63,15 @@ def add_fourier_noise(idx: Tuple[int, int],
         _, _, h, w = images.size()
     else:
         h, w = size
+
     noise = images.new_zeros(1, h, w, 2)
     noise[:, idx[0], idx[1]] = 1
-    noise[:, h - 1 - idx[0], w - 1 - idx[0]] = 1
+    noise[:, h - 1 - idx[0], w - 1 - idx[1]] = 1
     recon = ifft_shift(noise).irfft(2, normalized=True, onesided=False).unsqueeze(0)
+    recon.div_(recon.norm(p=2)).mul_(norm)
     if size is not None:
         recon = F.interpolate(recon, images.shape[2:])
-    images.add_(recon.div_(recon.norm(p=2)).mul_(norm))
+    images.add_(recon).clamp_(0, 1)
     return images
 
 
@@ -78,17 +80,39 @@ def fourier_map(model: nn.Module,
                 data: Tuple[Tensor, Tensor],
                 criterion: Callable[[Tensor, Tensor], Tensor],
                 norm: float,
-                fourier_map_size: Optional[Tuple[int, int]] = None
+                fourier_map_size: Optional[Tuple[int, int]] = None,
+                mean: Optional[List[float] or Tensor] = None,
+                std: Optional[List[float] or Tensor] = None
                 ) -> Tensor:
+    """
+
+    Args:
+        model: Trained model
+        data: Pairs of [input, target] to compute criterion
+        criterion: Criterion of (input, target) -> scalar value
+        norm: Intensity of fourier noise
+        fourier_map_size: Size of map, (H, W). Note that the computational time is dominated by HW.
+        mean: If the range of input is [-1, 1], specify mean and std.
+        std: If the range of input is [-1, 1], specify mean and std.
+
+    Returns:
+
+    """
     input, target = data
     if fourier_map_size is None:
         _, _, h, w = input.size()
     else:
         h, w = fourier_map_size
+    if mean is not None:
+        _mean = torch.as_tensor(mean, device=input.device, dtype=torch.float)
+        _std = torch.as_tensor(std, device=input.device, dtype=torch.float)
+        input = _denormalize(input, _mean, _std)  # [0, 1]
     map = torch.zeros(h, w)
     for u_i in tqdm(torch.triu_indices(h, w).t(), ncols=80):
-        l_i = u_i.flip(0)
+        l_i = h - 1 - u_i[0], w - 1 - u_i[1]
         noisy_input = add_fourier_noise(u_i, input, norm, fourier_map_size)
+        if mean is not None:
+            noisy_input = _normalize(noisy_input, _mean, _std)  # to [-1, 1]
         loss = _evaluate(model, (noisy_input, target), criterion)
         map[u_i[0], u_i[1]] = loss
         map[l_i[0], l_i[1]] = loss
--- anatome/utils.py
+++ anatome/utils.py
@@ -5,9 +5,11 @@ from torch import nn, Tensor
 
 AUTO_CAST = False
 
+
 def use_auto_cast():
     global AUTO_CAST
     AUTO_CAST = True
+
 
 def _evaluate(model: nn.Module,
               data: Tuple[Tensor, Tensor],
@@ -17,3 +19,23 @@ def _evaluate(model: nn.Module,
     with torch.cuda.amp.autocast(AUTO_CAST):
         input, target = data
         return criterion(model(input), target).item()
+
+
+def _normalize(input: Tensor,
+               mean: Tensor,
+               std: Tensor
+               ) -> Tensor:
+    # normalize tensor in [0, 1] to [-1, 1]
+    input = input.clone()
+    input.add_(-mean[:, None, None]).div_(std[:, None, None])
+    return input
+
+
+def _denormalize(input: Tensor,
+                 mean: Tensor,
+                 std: Tensor
+                 ) -> Tensor:
+    # denormalize tensor in [-1, 1] to [0, 1]
+    input = input.clone()
+    input.mul_(std[:, None, None]).add_(mean[:, None, None])
+    return input
