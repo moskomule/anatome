29c26edd40c0fa693773b1b34fa542177267ff70
moskomule <hataya@keio.jp>
2020-08-03 22:18:14 +0900

    fix precision of test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,6 +14,6 @@ def test_normalize_denormalize():
 
 def test_fft_shift():
     input = torch.randn(1, 3, 8, 8)
-    fft = utils.fft_shift(input.rfft(2, onesided=False))
-    ifft = utils.ifft_shift(fft).irfft(2, onesided=False)
-    assert torch.allclose(input, ifft)
+    fft = utils.fft_shift(input.rfft(2, normalized=True, onesided=False))
+    ifft = utils.ifft_shift(fft).irfft(2, normalized=True, onesided=False)
+    assert torch.allclose(input, ifft, atol=1e-6)
