0b041ab4be1b5e94ef45ce4a00a4e807bd7ba176
moskomule <hataya@keio.jp>
2020-12-28 18:19:35 +0900

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,19 +5,19 @@ from anatome import utils
 
 
 def test_normalize_denormalize():
-    input = torch.randn(1, 3, 5, 5)
+    input = torch.randn(4, 3, 5, 5)
     mean = torch.as_tensor([0.5, 0.5, 0.5])
     std = torch.as_tensor([0.5, 0.5, 0.5])
     output = utils._denormalize(utils._normalize(input, mean, std),
                                 mean, std)
-    assert torch.allclose(input, output)
+    assert torch.allclose(input, output, atol=1e-4)
 
 
 def test_fft_shift():
-    input = torch.randn(1, 3, 8, 8)
+    input = torch.randn(4, 3, 8, 8)
     fft = utils.fft_shift(input.rfft(2, normalized=True, onesided=False))
     ifft = utils.ifft_shift(fft).irfft(2, normalized=True, onesided=False)
-    assert torch.allclose(input, ifft, atol=1e-6)
+    assert torch.allclose(input, ifft, atol=1e-4)
 
 
 @pytest.mark.skipif(not utils.HAS_FFT_MODULE and hasattr(torch, "rfft"), reason="")
@@ -25,6 +25,6 @@ def test_fft_shift():
 @pytest.mark.parametrize("normalized", [False, True])
 @pytest.mark.parametrize("signal_ndim", [1, 2, 3])
 def test_rfft(signal_ndim, normalized, onesided):
-    input = torch.randn(1, 3, 8, 8)
+    input = torch.randn(4, 3, 8, 8)
     assert torch.allclose(input.rfft(signal_ndim, normalized, onesided),
                           utils._rfft(input, signal_ndim, normalized, onesided), atol=1e-4)
